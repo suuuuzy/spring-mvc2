0a7578fd95ee1fe0bd599cde79e45787f8638ee2
suuuuzy <jangsooji91@gmail.com>
2021-11-26 21:11:45 +0900

    요청 매핑

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -3,7 +3,7 @@ package hello.springmvc.basic;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Slf4j
@@ -12,7 +12,7 @@ public class LogTestController {
 
     private final Logger log = LoggerFactory.getLogger(getClass()); // @Slf4j 대체 가능
 
-    @GetMapping("/log-test")
+    @RequestMapping("/log-test")
     public String logTest() {
         String name = "Spring";
 
